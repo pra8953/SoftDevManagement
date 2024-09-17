38a0f123f6fe3139a8236b66ae9a9ec526a9ccfc
krishna1632 <krishna163208@gmail.com>
2024-09-18 00:09:27 +0530

    Bohot bada kaam shuru kiya hai

--- VocManagement/bootstrap/app.php
+++ VocManagement/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\UserMiddleware;
+use App\Http\Middleware\AdminMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        
+        $middleware->alias([
+            'userMiddleware' => UserMiddleware::class,
+            'adminMiddleware' => AdminMiddleware::class,
+        ]);
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- VocManagement/routes/web.php
+++ VocManagement/routes/web.php
@@ -3,13 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\AdminController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,3 +19,12 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::middleware(['auth','userMiddleware'])->group(function(){
+    Route::get('dashboard',[UserController::class,'index'])->name('dashboard');
+});
+
+Route::middleware(['auth','adminMiddleware'])->group(function(){
+    Route::get('/admin/dashboard',[AdminController::class,'index'])->name('admin.dashboard');
+});
