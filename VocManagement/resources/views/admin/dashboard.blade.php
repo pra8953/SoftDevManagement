083393c47055df0a34b34c34c36bb3c6da2013b7
krishna1632 <krishna163208@gmail.com>
2024-09-18 10:49:56 +0530

    asdfghjkl;

--- VocManagement/resources/views/admin/dashboard.blade.php
+++ VocManagement/resources/views/admin/dashboard.blade.php
@@ -1,24 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="dashboard.css">
-    <title>Document</title>
-    <style>
-        body{
-            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
-        }
-    </style>
-</head>
-<body>
-    <div id="wrapper">
-        <header>
-            <div class="header">
-                
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in as a user!") }}
+                    <h1>Hello</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque fugiat, placeat laudantium sequi nihil doloremque inventore sint quasi nesciunt repellat, commodi, soluta quo ipsa laboriosam labore ab aliquam perspiciatis vero.
+                    Voluptatibus delectus maxime dolor illum optio omnis ipsa ad ratione repudiandae quod, quibusdam aut sit consequuntur deserunt! Nihil excepturi magni odio, iure ducimus quae beatae, quas fugit, ullam cupiditate odit?
+                    Non, nostrum earum doloremque, quis facere excepturi dolore totam iusto tempora labore accusantium vel vero saepe voluptate ullam enim repellat impedit, rerum repellendus. Voluptate unde enim eos nostrum ducimus possimus!
+                    Neque beatae quis numquam et laboriosam, quo temporibus amet porro dignissimos cumque qui ad ab perspiciatis? Ea numquam ullam sequi id repellat perferendis! Laborum ab itaque dolor impedit vero veritatis.
+                    Aspernatur ex eos quia ad nobis officia, distinctio, fuga optio ea mollitia deleniti accusamus reprehenderit earum labore non facere adipisci impedit quas corrupti pariatur obcaecati harum. Ab dignissimos adipisci exercitationem?</p>
+                </div>
             </div>
-        </header>
+        </div>
     </div>
-</body>
-</html>
+</x-app-layout>
+
+
+
